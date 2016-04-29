e5c587dcc5c56ab0dbffd305f05d063ca06df5d0
Lawrence Lau <m@torolab.hk>
2016-04-29 16:13:39 +0800

    v0.1

---
+++ spec/chinese-converter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ChineseConverterView from '../lib/chinese-converter-view';
+
+describe('ChineseConverterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
